3126a3c727865305bb10000978d4192f7d19bfe5
Ian Cullinane <ian.cullinane@gmail.com>
2019-06-06 10:34:50 -0400

    works without consul-client

--- deploy.tf
+++ deploy.tf
@@ -12,39 +12,39 @@ provider "digitalocean" {
 }
 
 
-resource "digitalocean_droplet" "gateway" {
-  image = "coreos-stable"
-  name = "gateway-server-${format(count.index)}"
-  region = "${var.do_region}"
-  private_networking = true
-  size = "${var.size_worker}"
-  ssh_keys = ["${split(",", var.ssh_fingerprint)}"]
-  count = "1"
-  tags   = ["consul", "gateway"]
-  // outputting the ignition looks like this: `ct < consul.yaml > ./output/ignition.json`
-  user_data = "${file("ct/output/gateway.json")}"
-
-  // provisioner "file" {
-  //   source      = "traefik/traefik.toml"
-  //   destination = "/opt/traefik.toml"
-  // }
-}
-
-// resource "digitalocean_droplet" "consul_master" {
+// resource "digitalocean_droplet" "gateway" {
 //   image = "coreos-stable"
-//   name = "consul-server-${format(count.index)}"
+//   name = "gateway-server-${format(count.index)}"
 //   region = "${var.do_region}"
 //   private_networking = true
-//   size = "${var.size_master}"
+//   size = "${var.size_worker}"
 //   ssh_keys = ["${split(",", var.ssh_fingerprint)}"]
-//   count = "2"
-//   tags   = ["consul"]
-//   user_data = "${file("ct/output/consul.json")}"
+//   count = "1"
+//   tags   = ["consul", "gateway"]
+//   // outputting the ignition looks like this: `ct < consul.yaml > ./output/ignition.json`
+//   user_data = "${file("ct/output/gateway.json")}"
+
+//   // provisioner "file" {
+//   //   source      = "traefik/traefik.toml"
+//   //   destination = "/opt/traefik.toml"
+//   // }
 // }
 
+resource "digitalocean_droplet" "consul_master" {
+  image = "coreos-stable"
+  name = "consul-server-${format(count.index)}"
+  region = "${var.do_region}"
+  private_networking = true
+  size = "${var.size_master}"
+  ssh_keys = ["${split(",", var.ssh_fingerprint)}"]
+  count = "2"
+  tags   = ["consul"]
+  user_data = "${file("ct/output/consul.json")}"
+}
 
 
-// resource "digitalocean_loadbalancer" "public" {
+
+// resource "digitalocean_loadbalancer" "lb_public" {
 //   name = "lb"
 //   region = "nyc3"
 
@@ -65,14 +65,14 @@ resource "digitalocean_droplet" "gateway" {
 // }
 
 
-// output "cluster-private-ips" {
-//   value = "${formatlist("%v", digitalocean_droplet.consul_master.*.ipv4_address_private)}"
-// }
-
-// output "cluster-public-ips" {
-//   value = "${formatlist("ssh core@%v", digitalocean_droplet.consul_master.*.ipv4_address)}"
-// }
-
-output "gateway-public-ips" {
-  value = "${formatlist("ssh core@%v", digitalocean_droplet.gateway.*.ipv4_address)}"
+output "cluster-private-ips" {
+  value = "${formatlist("%v", digitalocean_droplet.consul_master.*.ipv4_address_private)}"
 }
+
+output "cluster-public-ips" {
+  value = "${formatlist("ssh core@%v", digitalocean_droplet.consul_master.*.ipv4_address)}"
+}
+
+// output "gateway-public-ips" {
+//   value = "${formatlist("ssh core@%v", digitalocean_droplet.gateway.*.ipv4_address)}"
+// }
